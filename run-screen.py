5a31bcec878032c4f729bf847a8666d4f1818bf1
Kyle Peeler <kylepeeler@gmail.com>
2018-11-30 11:08:52 -0500

    changed section indexes to be zero based (1, 2, 3, 4 -> 0, 1, 2, 3)

--- run-screen.py
+++ run-screen.py
@@ -19,38 +19,38 @@ weatherString = "Temp: " + str(weatherTemp) + "F " + weatherStatus.lower() + " i
 class RunScreen(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunScreen, self).__init__(*args, **kwargs)
+        self.scrollPos0 = 32
         self.scrollPos1 = 32
         self.scrollPos2 = 32
         self.scrollPos3 = 32
-        self.scrollPos4 = 32
         self.start = datetime.datetime.now()
         self.font = graphics.Font()
         self.font.LoadFont('rpi-rgb-led-matrix/fonts/tom-thumb.bdf')
         self.time = datetime.datetime.now()
 
     def scroll(self, secInd, length):
-        if (secInd >= 1 and secInd <= 4 and length > 32):
+        if (secInd >= 0 and secInd <= 3 and length > 32):
             scrollPos = getattr(self, "scrollPos" + str(secInd));
             setattr(self, "scrollPos" + str(secInd), scrollPos - 1); 
             if (scrollPos - 1 + length < 0):
                setattr(self, "scrollPos" + str(secInd), 32);
-        elif (secInd >= 1 and secInd <= 4 and length <= 32):
+        elif (secInd >= 0 and secInd <= 3 and length <= 32):
             setattr(self, "scrollPos" + str(secInd), 0)
     
     def getSectionHeight(self, sectionIndex):
-        if (sectionIndex == 1):
+        if (sectionIndex == 0):
             return 9;
-        elif (sectionIndex == 2):
+        elif (sectionIndex == 1):
             return 16;
-        elif (sectionIndex == 3):
+        elif (sectionIndex == 2):
             return 23;
-        elif (sectionIndex == 4):
+        elif (sectionIndex == 3):
             return 30;
         else:
             return 32;
     
     def displayText(self, canvas, text, color, xOffset, secInd):
-        if (secInd >= 1 and secInd <= 4):
+        if (secInd >= 0 and secInd <= 3):
             height = self.getSectionHeight(secInd)
             self.scroll(secInd, graphics.DrawText(canvas, self.font, xOffset + getattr(self, "scrollPos" + str(secInd)), height, color, text))
     
@@ -68,10 +68,10 @@ class RunScreen(SampleBase):
             canvas = self.matrix.CreateFrameCanvas()
             canvas.Clear()
         
-            self.displayClock(canvas, graphics.Color(255, 165, 0), 1)
-            self.displayWeather(canvas, graphics.Color(0, 255, 0), 2)
-            self.displayText(canvas, "Example text", graphics.Color(0, 255, 255), 3, 3)
-            self.displayDate(canvas, graphics.Color(255, 0, 0), 4)
+            self.displayClock(canvas, graphics.Color(255, 165, 0), 0)
+            self.displayWeather(canvas, graphics.Color(0, 255, 0), 1)
+            self.displayText(canvas, "Example text", graphics.Color(0, 255, 255), 3, 2)
+            self.displayDate(canvas, graphics.Color(255, 0, 0), 3)
 
             time.sleep(0.05)
             canvas = self.matrix.SwapOnVSync(canvas)
